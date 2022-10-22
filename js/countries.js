c36e94a1118b5b2a1efe0d89c43e5cabbee1eeb4
asifamanapon <aponasifaman@gmail.com>
2022-10-22 18:21:37 +0600

    eight-commit

--- js/countries.js
+++ js/countries.js
@@ -20,6 +20,18 @@ const displayCountries = countries => {
     })
 }
 const loadCountriesDetail = (code) => {
-    console.log('get country detail', code)
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    // console.log('get country detail', code)
+    fetch(url)
+        .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+}
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDetail = document.getElementById('country-detail');
+    countryDetail.innerHTML = `
+        <h2>Details: ${country.name.common}</h2>
+        <img src="${country.flags.png}">
+    `
 }
 loadCountries()
